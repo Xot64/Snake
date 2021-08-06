e2a90c0152b162cf0b4754a7bedfa12504831141
XotGames <hdv_1990@mail.ru>
2021-08-06 16:29:15 +0300

    Food Spawner and ColorSettings

---
+++ Assets/Scripts/C_Settings.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C_Settings: MonoBehaviour 
+{
+    public Color[] colors = { Color.red, Color.green, Color.blue,Color.yellow, Color.cyan, Color.magenta };
+    public Color snakeColor;
+    public Color badFoodColor;
+    private void Awake()
+    {
+        SetColor(GetColor());
+    }
+
+    public void SetColor(Color c)
+    {
+        snakeColor = c;
+        badFoodColor = c;
+        while (badFoodColor == snakeColor)
+        {
+            badFoodColor = GetColor();
+        }
+    }
+    public void SetColor (int i)
+    {
+        SetColor(colors[i]);
+    }
+
+    public Color GetColor(int i)
+    {
+        return colors[Mathf.Min(Mathf.Max(i,0),colors.Length - 1)];
+    }
+    public Color GetColor()
+    {
+        return GetColor(Random.Range(0,colors.Length));
+    }
+    public Color GetColor(bool snake)
+    {
+        return snake ? snakeColor : badFoodColor;
+    }
+}
