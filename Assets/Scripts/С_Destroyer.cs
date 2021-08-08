60d7ec08726b968e738edd2c4a211f245930190c
XotGames <hdv_1990@mail.ru>
2021-08-08 13:17:36 +0300

    Destroyer

---
+++ Assets/Scripts/С_Destroyer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class С_Destroyer : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if ((other.tag == "Gem") || (other.tag == "Food") || (other.tag == "Bomb"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
