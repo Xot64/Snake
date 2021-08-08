ecb8aabbae3e4a477ca372736be1ecf97bc0fca8
XotGames <hdv_1990@mail.ru>
2021-08-08 09:43:58 +0300

    Всасывание еды

---
+++ Assets/Scripts/C_Vacuum.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C_Vacuum : MonoBehaviour
+{
+    public float power = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Food")
+        {
+            Vector3 direct = (other.transform.position - transform.position).normalized;
+            other.transform.position -= direct * power * Time.deltaTime;
+        }
+    }
+}
